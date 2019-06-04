aa52cf84a4bc8fcd9aa84ada2309ea0855c9577b
Malyutka Kobanova <tankettedawn@gmail.com>
2019-06-04 12:52:05 -0400

    it sort of worked...sort of

--- palindrome.py
+++ palindrome.py
@@ -1,6 +1,20 @@
 # gonna try a couple different strategies here and see if all of them work
 
+# function to reverse a string
 def reverse_string(str):
     return str[::-1]
 
-print(reverse_string("yavanna"))
+# For testing use only
+#print(reverse_string("yavanna"))
+
+#text_input = input("Input some text: ")
+#text_reverse = reverse_string(text_input)
+
+# For testing use only
+#print(text_reverse)
+
+#if text_input == text_reverse:
+#    print("This is a palindrome!")
+
+#else:
+#    print("This is not a palindrome.")
